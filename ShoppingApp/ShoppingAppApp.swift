77001eabb04a0639808293fd821644fedea75abd
CeciliaChen <cecilia@pdotf.com>
2024-01-03 21:21:43 -0500

    Initial Commit

---
+++ ShoppingApp/ShoppingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingAppApp.swift
+//  ShoppingApp
+//
+//  Created by Cecilia Chen on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
